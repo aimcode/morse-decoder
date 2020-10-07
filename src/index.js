9a70858d1920b7e976d62322e5aeb33267a2f460
aimcode <aimcode2020@gmail.com>
2020-10-07 21:28:06 +0500

    feat: add solution for morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+    let size = expr.length / 10
+
+    for (let i = 0, j = 0 ; i < size; i++, j+=10) {
+        arr.push(expr.slice(j, j+10))
+    }
+
+    arr = arr.map(el => {
+        let str = ""
+
+        if(el.includes("*")) return " "
+        for(let i = 0; i < el.length; i += 2) {
+            let subStr = el.slice(i, i+2)
+
+            switch (subStr) {
+                case "10":
+                    str += "."
+                    break
+                case "11":
+                    str += "-"
+                    break
+                default:
+                    str += ""
+            }
+        }
+
+        return MORSE_TABLE[str]
+    })
+
+    return arr.join("")
 }
 
 module.exports = {
